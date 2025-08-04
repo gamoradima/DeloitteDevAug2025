aa99347d4e22894fdae22966b18d6f807e231ccc
gamoradima <gamoradima@gmail.com>
2025-08-04 18:26:18 +0300

    ScriptTask example to calc average ticket price

---
+++ UsrYacht/Autogenerated/Src/UsrEncryptedDemoProcess.UsrYacht.cs
@@ -0,0 +1,44 @@
+﻿namespace Terrasoft.Core.Process
+{
+
+	using System;
+	using System.Collections.Generic;
+	using System.Collections.ObjectModel;
+	using System.Drawing;
+	using System.Globalization;
+	using System.Text;
+	using Terrasoft.Common;
+	using Terrasoft.Core;
+	using Terrasoft.Core.Configuration;
+	using Terrasoft.Core.DB;
+	using Terrasoft.Core.Entities;
+	using Terrasoft.Core.Process;
+	using Terrasoft.Core.Process.Configuration;
+
+	#region Class: UsrEncryptedDemoProcessMethodsWrapper
+
+	/// <exclude/>
+	public class UsrEncryptedDemoProcessMethodsWrapper : ProcessModel
+	{
+
+		public UsrEncryptedDemoProcessMethodsWrapper(Process process)
+			: base(process) {
+			AddScriptTaskMethod("ScriptTask1Execute", ScriptTask1Execute);
+		}
+
+		#region Methods: Private
+
+		private bool ScriptTask1Execute(ProcessExecutingContext context) {
+			string pw = Core.Configuration.SysSettings.GetValue(UserConnection, "TopSecretMilitaryPassword", string.Empty);
+			Set("MyParam", pw);
+			return true;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
+
---
+++ UsrYacht/Schemas/UsrAverageTicketPricePage/UsrAverageTicketPricePage.js
@@ -0,0 +1,118 @@
+define("UsrAverageTicketPricePage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "Tabs",
+				"values": {
+					"styleType": "default",
+					"mode": "tab",
+					"bodyBackgroundColor": "primary-contrast-500",
+					"selectedTabTitleColor": "auto",
+					"tabTitleColor": "auto",
+					"underlineSelectedTabColor": "auto",
+					"headerBackgroundColor": "auto"
+				}
+			},
+			{
+				"operation": "merge",
+				"name": "GridContainer_uxln7d4",
+				"values": {
+					"gap": {
+						"columnGap": "large",
+						"rowGap": "none"
+					},
+					"visible": true,
+					"color": "transparent",
+					"borderRadius": "none",
+					"padding": {
+						"top": "none",
+						"right": "none",
+						"bottom": "none",
+						"left": "none"
+					},
+					"alignItems": "stretch"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "AvgPrice",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.NumberInput",
+					"label": "$Resources.Strings.PageParameters_UsrIntegerParameter1_870rt96",
+					"labelPosition": "auto",
+					"control": "$PageParameters_UsrIntegerParameter1_870rt96",
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "SQLText",
+				"values": {
+					"layoutConfig": {
+						"column": 2,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 5
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_8imjxy5",
+					"labelPosition": "auto",
+					"control": "$PageParameters_UsrTextParameter1_8imjxy5",
+					"multiline": true,
+					"visible": true,
+					"readonly": true,
+					"placeholder": "",
+					"tooltip": ""
+				},
+				"parentName": "GridContainer_uxln7d4",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrIntegerParameter1_870rt96": {
+						"modelConfig": {
+							"path": "PageParameters.UsrAvgTicketPrice"
+						}
+					},
+					"PageParameters_UsrTextParameter1_8imjxy5": {
+						"modelConfig": {
+							"path": "PageParameters.UsrSqlText"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
